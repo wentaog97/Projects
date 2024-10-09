43775c19db941d028732ead4b9c92b55aee1d510
Wentao Gao <wentaog@hotmail.com>
2024-10-09 03:50:18 -0700

    Updated GA-TSP

--- GA-TravelingSalesman/EvalXOverMutate.cpp
+++ GA-TravelingSalesman/EvalXOverMutate.cpp
@@ -7,21 +7,16 @@
 #include "Timer.h"
 #include "Trip.h"
 
-// DEBUG
-#define TEST_CHROMOSOMES_N 12
-#define TEST_TOP_X         6
-// DEBUG
-
 #define CHROMOSOMES    50000 // 50000 different trips
 #define CITIES         36    // 36 cities = ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 #define TOP_X          25000 // top optimal 25%
-#define MUTATE_RATE    50    // optimal 50%
+#define MUTATE_RATE    52    // optimal 50%
 
 using namespace std;
 
-// HELPERS
+// HELPERS FUNCTIONS
 bool CompareTrips(const Trip& a, const Trip& b) {
-    return a.fitness < b.fitness; 
+   return a.fitness < b.fitness; 
 }
 
 int getIndex(char c){
@@ -34,18 +29,20 @@ char getCityCh(int index){
 }
 
 char getRandomCity() {
-  char city = rand( ) % CITIES;
-  if ( city < 26 )
-    city += 'A';
-  else
-    city = city - 26 + '0';
-  return city;
+   char city = rand() % CITIES;
+   if ( city < 26 ) return city + 'A';
+   return city - 26 + '0';
 }
 
 float getDistance(char a, char b, int coordinates[CITIES][2]){
    int ia = getIndex(a);
    int ib = getIndex(b);
    return sqrt(pow(coordinates[ia][0] - coordinates[ib][0], 2) + pow(coordinates[ia][1] - coordinates[ib][1], 2));
+}
+
+float getOriDistance(char b, int coordinates[CITIES][2]){
+   int ib = getIndex(b);
+   return sqrt(pow(0 - coordinates[ib][0], 2) + pow(0 - coordinates[ib][1], 2));
 }
 
 void getComplement(const char* ori, char* tar){
@@ -56,73 +53,6 @@ void getComplement(const char* ori, char* tar){
       tar[i]=compliment[index];
    }
 }
-// HELPERS
-
-
-/*
- * Evaluates each trip (or chromosome) and sort them out
-
-   Load in the chromosomes, coordinates
-   we need to findout the value of the chromosomes
-
-   Evaluates the distance of each trip and sorts out all the trips in the shortest-first
-   order. Memorize the current shortest trip as a tentative answer if it is shorter the
-   previous.
- */
-void evaluate( Trip trip[CHROMOSOMES], int coordinates[CITIES][2] ) {
-   // cout << "Evaluating" << endl;
-
-   // Iterating through all trips
-   #pragma omp parallel
-   {
-      #pragma omp for
-      for(int i_c = 0; i_c < CHROMOSOMES; i_c++){
-         char prevCity = trip[i_c].itinerary[0];
-         double distance = 0;
-
-         // Start with the second city, and calculate its distance from the prev
-         for(int i=1; i<CITIES; i++){
-            char city = trip[i_c].itinerary[i];
-            float d = getDistance(city, prevCity, coordinates);
-            distance += d;
-            prevCity = city;
-         }
-
-         // After reaching the last city, calculate the last to the first city.
-         char city = trip[i_c].itinerary[0];
-         float d = getDistance(city, prevCity, coordinates);
-         distance += d;
-         
-         // Assign the fitness of this trip with the total calculated distance
-         trip[i_c].fitness = distance;
-      }  
-   }
-
-   // Sort all trips based on distance using a custome comparator
-   std::sort(trip, trip+CHROMOSOMES, CompareTrips);
-   
-   // DEBUG
-   if(DEBUG){
-      for(int i=0; i<CHROMOSOMES; i++){
-         for(int index=0; index<CITIES; index++){
-            cout << trip[i].itinerary[index];
-         }
-         cout << endl;
-         cout << trip[i].fitness << endl;
-      }
-   }
-   // DEBUG
-}
-
-/*
- * Generates new TOP_X offsprings from TOP_X parents.
- * Noe that the i-th and (i+1)-th offsprings are created from the i-th and (i+1)-th parents
- */
-
- /*
-    Generates 25,000 off-springs from the parents. More specifically, we spawn a pair 
-    of child[i] and [i+1] from parent[i] and [i+1] 
-  */
 
 char getNextCity(const char* chromosome, char c){
    int index = -1;
@@ -135,78 +65,22 @@ char getNextCity(const char* chromosome, char c){
    return chromosome[index+1];
 }
 
-void crossover( Trip parents[TOP_X], Trip offsprings[TOP_X], int coordinates[CITIES][2] ) {
-   // cout << "Crossing over" << endl;
-   #pragma omp parallel
-   {
-      #pragma omp for
-      for(int i=0; i<TOP_X; i+=2){
-         Trip p1 = parents[i];
-         Trip p2 = parents[i+1];
-
-         // Hash set for visited cities
-         bool visited[36] = {false};
-         
-         offsprings[i].itinerary[0] = p1.itinerary[0];
-         visited[getIndex(offsprings[i].itinerary[0])] = true;
-         
-         // Greedy cross over
-         for(int j=1; j<36; j++){
-            char startingCity = offsprings[i].itinerary[j-1];
-
-            char p1Next = getNextCity(p1.itinerary, startingCity);
-            char p2Next = getNextCity(p2.itinerary, startingCity);
-
-            float d1 = getDistance(startingCity, p1Next, coordinates);
-            float d2 = getDistance(startingCity, p2Next, coordinates);
-
-            if(min(d1,d2) == d1){
-               if(!visited[getIndex(p1Next)]) {
-                  offsprings[i].itinerary[j] = p1Next;
-                  visited[getIndex(p1Next)] = true;
-               } else if(!visited[getIndex(p2Next)]){
-                  offsprings[i].itinerary[j] = p2Next;
-                  visited[getIndex(p2Next)] = true;
-               } else {
-                  char randomCity = getRandomCity();
-                  while(visited[getIndex(randomCity)]){
-                     randomCity = getRandomCity();
-                  }
-                  offsprings[i].itinerary[j] = randomCity;
-                  visited[getIndex(randomCity)] = true;
-               }
-            } else {
-               if(!visited[getIndex(p2Next)]) {
-                  offsprings[i].itinerary[j] = p2Next;
-                  visited[getIndex(p2Next)] = true;
-               } else if(!visited[getIndex(p1Next)]){
-                  offsprings[i].itinerary[j] = p1Next;
-                  visited[getIndex(p1Next)] = true;
-               } else {
-                  char randomCity = getRandomCity();
-                  while(visited[getIndex(randomCity)]){
-                     randomCity = getRandomCity();
-                  }
-                  offsprings[i].itinerary[j] = randomCity;
-                  visited[getIndex(randomCity)] = true;
-               }
-            }
-         }
-
-         getComplement(offsprings[i].itinerary, offsprings[i+1].itinerary);      
-      }
+char getValidCity(char p1Next, char p2Next, bool* visited) {
+   if (!visited[getIndex(p1Next)]) {
+      visited[getIndex(p1Next)] = true;
+      return p1Next;
    }
 
+   if (!visited[getIndex(p2Next)]) {
+      visited[getIndex(p2Next)] = true;
+      return p2Next;
+   }
+
+   char randomCity = getRandomCity();
+   while (visited[getIndex(randomCity)]) randomCity = getRandomCity(); 
+   visited[getIndex(randomCity)] = true;
+   return randomCity;
 }
-
-/*
- * Mutate a pair of genes in each offspring.
- */
-
- /*
-    Randomly chooses two distinct cities (or genes) in each trip (or chromosome) with a
-    given probability, and swaps them.
- */
 
 void swap(char * arr, int a, int b){
    char temp = arr[a];
@@ -214,18 +88,111 @@ void swap(char * arr, int a, int b){
    arr[b] = temp;
 }
 
-void mutate( Trip offsprings[TOP_X] ) {
-   // cout << "Mutating" << endl;
+
+/*
+ * Evaluates each trip (or chromosome) and sort them out
+ */
+void evaluate( Trip trip[CHROMOSOMES], int coordinates[CITIES][2] ) {
+   // Precompute distances between cities
+   float distanceMatrix[CITIES][CITIES];
+   #pragma omp parallel for collapse(2)
+   for (int i = 0; i < CITIES; i++) {
+      for (int j = 0; j < CITIES; j++) {
+         distanceMatrix[i][j] = sqrt(pow(coordinates[i][0] - coordinates[j][0], 2) + pow(coordinates[i][1] - coordinates[j][1], 2));
+      }
+   }
+
+   // Iterating through all trips
+   #pragma omp parallel for
+   for(int i_c = 0; i_c < CHROMOSOMES; i_c++){
+      char prevCity = trip[i_c].itinerary[0];
+      double distance = getOriDistance(prevCity, coordinates);
+      int prevCityIndex = getIndex(trip[i_c].itinerary[0]);
+
+      // Start with the second city, and calculate its distance from the prev
+      for (int i = 1; i < CITIES; i++) {
+         int currentCityIndex = getIndex(trip[i_c].itinerary[i]);
+         distance += distanceMatrix[prevCityIndex][currentCityIndex];
+         prevCityIndex = currentCityIndex;
+      }
+      
+      // Assign the fitness of this trip with the total calculated distance
+      trip[i_c].fitness = distance;
+   }  
+
+   // Sort all trips based on distance using a custome comparator
+   std::sort(trip, trip+CHROMOSOMES, CompareTrips);
+}
+
+/*
+ * Generates new TOP_X offsprings from TOP_X parents.
+ * Noe that the i-th and (i+1)-th offsprings are created from the i-th and (i+1)-th parents
+ */
+void crossover( Trip parents[TOP_X], Trip offsprings[TOP_X], int coordinates[CITIES][2] ) {
+   
+   // Precompute distances between cities
+   float distanceMatrix[CITIES][CITIES];
+   #pragma omp parallel for collapse(2)
+   for (int i = 0; i < CITIES; i++) {
+      for (int j = 0; j < CITIES; j++) {
+         distanceMatrix[i][j] = sqrt(pow(coordinates[i][0] - coordinates[j][0], 2) + pow(coordinates[i][1] - coordinates[j][1], 2));
+      }
+   }
+
    #pragma omp parallel
    {
+      // Generate a different seed for each thread
+      srand(time(NULL) + omp_get_thread_num());
+
       #pragma omp for
-      for(int i=0; i<TOP_X; i++){
-         int a=0, b=0;
-         a = rand()%CITIES;
-         b = rand()%CITIES;
-         while(b==a) b=rand()%CITIES;
-         int prob = rand()%100;
-         if(prob > MUTATE_RATE) swap(offsprings[i].itinerary, a, b);
+      for(int i=0; i<TOP_X; i+=2){
+         // Hash set for visited cities
+         bool visited[36] = {false};
+         
+         offsprings[i].itinerary[0] = parents[i].itinerary[0];
+         visited[getIndex(offsprings[i].itinerary[0])] = true;
+         
+         // Greedy cross over
+         for(int j=1; j<36; j++){
+            char startingCity = offsprings[i].itinerary[j-1];
+
+            char p1Next = getNextCity(parents[i].itinerary, startingCity);
+            char p2Next = getNextCity(parents[i+1].itinerary, startingCity);
+
+            float d1 = distanceMatrix[getIndex(startingCity)][getIndex(p1Next)];
+            float d2 = distanceMatrix[getIndex(startingCity)][getIndex(p2Next)];
+
+            if (d1 <= d2) {
+               offsprings[i].itinerary[j] = getValidCity(p1Next, p2Next, visited);
+            } else {
+               offsprings[i].itinerary[j] = getValidCity(p2Next, p1Next, visited);
+            }
+         }
+
+         // Generate complement 
+         getComplement(offsprings[i].itinerary, offsprings[i+1].itinerary);      
+      }
+   }
+}
+
+/*
+ * Mutate a pair of genes in each offspring.
+ */
+void mutate( Trip offsprings[TOP_X] ) {
+   #pragma omp parallel
+   {  
+      // Generate a different seed for each thread
+      srand(time(NULL) + omp_get_thread_num());
+
+      #pragma omp for 
+      for (int i = 0; i < TOP_X; i++) {
+         int prob = rand() % 100;
+         if (prob < MUTATE_RATE){
+            int a = rand() % CITIES;  
+            int b = rand() % CITIES;
+            while (b == a) b = rand() % CITIES; 
+            swap(offsprings[i].itinerary, a, b);  
+         }
       }
    }
 }
